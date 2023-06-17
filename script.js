6b2260070a2b870a8581fdcc14683aed9953d9ef
Parshva Gang <coderphilosophy09@gmail.com>
2023-06-17 12:42:30 +0530

    Add erase button

--- script.js
+++ script.js
@@ -1,38 +1,56 @@
-let num = 3;
-let length = 500 / num;
-console.log(length);
-const grid = document.querySelector('.grid');
-const reset = document.querySelector('.reset');
+/*let grid = document.querySelector('.grid');
+let reset = document.querySelector('.reset');
+let input = document.querySelector('.input');
+let erase = document.querySelector('.erase');
+let gridElement;
 
-for(let i = 0; i < num; i++){
-    for(let j = 0; j < num; j++){
-        const content = document.createElement('div');
-        content.classList.add('gridElement');
-        content.style.backgroundColor = "black";
-        content.style.boxSizing = "border-box";
-        content.style.height = length + "px";
-        content.style.width = length + "px";
-        console.log(length);
-        content.style.border = "2px solid red";
-        
-        grid.appendChild(content);
-        
+let num = 10;
+let length = 500 / num;
+createGrid(num, length)
+
+input.addEventListener('input', function(e){
+    num = e.target.value;
+    length = 500 / num;
+    clearGrid();
+    createGrid(num, length);
+})
+
+function createGrid(num, length){
+    for(let i = 0; i < num; i++){
+        for(let j = 0; j < num; j++){
+            const content = document.createElement('div');
+            content.classList.add('gridElement');
+            content.style.backgroundColor = "black";
+            content.style.boxSizing = "border-box";
+            content.style.height = length + "px";
+            content.style.width = length + "px";
+            content.style.border = "1px solid red";
+            content.addEventListener('mouseover', changeColor);
+            grid.appendChild(content);
+            
+        }
+    }
+    gridElement =  document.querySelectorAll('.gridElement');
+    grid = document.querySelector('.grid');
+    console.log(gridElement[1]);
+}
+
+function clearGrid(){
+    grid.innerHTML = "";
+}
+
+let mouse = false;
+document.body.onmousedown = () => {mouse = true};
+document.body.onmouseup = () => {mouse = false};
+
+function changeColor(e){
+    console.log(e);
+    if(mouse){
+        e.target.style.backgroundColor = "blue";
     }
 }
 
-
-const gridElement = document.querySelectorAll('.gridElement');
-console.log(gridElement[1]);
-
-let mouse = false;
-grid.addEventListener('mousedown', () => {
-    mouse = true;
-})
-grid.addEventListener('mouseup', () => {
-    mouse = false;
-})
-
-
+/*
 gridElement.forEach((element) => {
     element.addEventListener('mousemove', () => {
         if(mouse){
@@ -50,10 +68,94 @@ function resetGrid(){
     })
     mouse = false;
 }
+*/
 
 
+let grid = document.querySelector('.grid');
+let input = document.querySelector('.input');
+let blue = document.querySelector('.blue');
+let reset = document.querySelector('.reset');
+let erase = document.querySelector('.erase');
 
 
+let gridSize = 10;
+let length = 500 / gridSize;
+let colourChoice = 'blue';
+let mouse = false;
+
+document.body.onmousedown = () => {mouse = true};
+document.body.onmouseup = () => {mouse = false};
+
+erase.onclick = () => setColourChoice('erase');
+blue.onclick = () => setColourChoice('blue');
+
+input.addEventListener('input', function(e){
+    num = e.target.value;
+    length = 500 / num;
+    clearGrid();
+    createGrid(num, length);
+})
+
+function setColourChoice(choice){
+    if(choice == 'erase'){
+        selectButton('erase');
+        colourChoice = 'erase';
+    }
+    else if(choice == 'blue'){
+        selectButton('blue');
+        colourChoice = 'blue';
+    }
+}
+
+function selectButton(choice){
+    if(colourChoice == 'blue'){
+        blue.classList.remove('active');
+    }
+    else if(colourChoice == 'erase'){
+        erase.classList.remove('active');
+    }
+
+    if(choice == 'blue'){
+        blue.classList.add('active');
+    }
+    else if(choice == 'erase'){
+        erase.classList.add('active');
+    }
+}
+
+
+function createGrid(gridSize, length){
+    for(let i = 0; i < gridSize*gridSize; i++){
+        let content = document.createElement('div');
+        content.classList.add('gridElement');
+        content.style.backgroundColor = "black";
+        content.style.boxSizing = "border-box";
+        content.style.height = length + "px";
+        content.style.width = length + "px";
+        content.style.border = "1px solid red";
+
+        content.addEventListener('mouseover', changeColor);
+        grid.appendChild(content);
+    }
+}
+
+
+function clearGrid(){
+    grid.innerHTML = "";
+}
+
+function changeColor(e){
+    if(mouse && colourChoice == 'blue'){
+        e.target.style.backgroundColor = "blue";
+    }
+    if(mouse && colourChoice == 'erase'){
+        e.target.style.backgroundColor = "black";
+    }
+}
+
+window.onload = () => {
+    createGrid(10, 50);
+}
 
 
 
